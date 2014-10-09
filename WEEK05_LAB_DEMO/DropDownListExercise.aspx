7809300aaaa349e2e7f56966d85317f011e38c53
ningboliuwei <liuwei@nbu.edu.cn>
2014-10-09 12:01:15 +0800

    增加了第五周上机实验下拉列表的部分

---
+++ WEEK05_LAB_DEMO/DropDownListExercise.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DropDownListExercise.aspx.cs"
+    Inherits="WEEK05_LAB_DEMO_DropDownListExercise" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>无标题页</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>
+            要添加的项目的Text属性值
+            <asp:TextBox ID="txtItemText" runat="server" Width="208px" Height="24px" MaxLength="20"></asp:TextBox></p>
+        <p>
+            要添加的项目的Value属性值
+            <asp:TextBox ID="txtItemValue" runat="server" Width="208px" Height="24px" MaxLength="5"></asp:TextBox></p>
+        <p>
+            <asp:DropDownList ID="dplItems" runat="server" Width="208px" AutoPostBack="True"
+                OnSelectedIndexChanged="dplItems_SelectedIndexChanged">
+            </asp:DropDownList>
+        </p>
+        <p>
+            <asp:Label ID="lblInfo" runat="server" ForeColor="#C00000"></asp:Label></p>
+        <p>
+            <asp:Button ID="btnAdd" runat="server" Text="添加" Width="80px" Height="24px" OnClick="btnAdd_Click">
+            </asp:Button>
+            <asp:Button ID="btnDelete" runat="server" Text="删除当前项" Width="80px" Height="24px"
+                OnClick="btnDelete_Click"></asp:Button>
+            <asp:Button ID="btnClear" runat="server" Text="清除所有项" Width="80px" Height="24px"
+                OnClick="btnClear_Click"></asp:Button>
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WEEK05_LAB_DEMO/DropDownListExercise.aspx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class WEEK05_LAB_DEMO_DropDownListExercise : System.Web.UI.Page
+{
+	protected void Page_Load(object sender, System.EventArgs e)
+	{
+		// 在此处放置用户代码以初始化页面
+		DisplayInfoAndSetButtonEnabled();
+	}
+
+	#region Web 窗体设计器生成的代码
+	override protected void OnInit(EventArgs e)
+	{
+		//
+		// CODEGEN: 该调用是 ASP.NET Web 窗体设计器所必需的。
+		//
+		InitializeComponent();
+		base.OnInit(e);
+	}
+
+	/// <summary>
+	/// 设计器支持所需的方法 - 不要使用代码编辑器修改
+	/// 此方法的内容。
+	/// </summary>
+	private void InitializeComponent()
+	{
+
+	}
+	#endregion
+
+	protected void btnAdd_Click(object sender, System.EventArgs e)
+	{
+		//如果Text文本框输入的数据为空
+		if (txtItemText.Text.Trim() == "")
+		{
+			//显示错误提示
+			lblInfo.Text ="Text值不能为空";
+		}
+		//如果Value文本框输入的数据为空
+		else if (txtItemValue.Text.Trim() == "")
+		{
+			lblInfo.Text = "Value值不能为空";
+		}
+		else
+		{
+			//创建一个新的选项
+			ListItem li = new ListItem();
+
+			li.Text = txtItemText.Text.Trim();
+			li.Value = txtItemValue.Text.Trim();
+
+			//将选项添加到下拉列表中
+			dplItems.Items.Add(li);
+
+			//选中最后一个选项（即新添加进的选项）
+			dplItems.SelectedIndex = dplItems.Items.Count - 1;
+
+			//显示信息并设置按钮可用状态
+			DisplayInfoAndSetButtonEnabled();
+
+			//清空Text与Value文本框
+			txtItemText.Text = "";
+			txtItemValue.Text = "";
+		}
+	}
+
+	protected void dplItems_SelectedIndexChanged(object sender, System.EventArgs e)
+	{
+		DisplayInfoAndSetButtonEnabled();
+
+	}
+
+	protected void btnDelete_Click(object sender, System.EventArgs e)
+	{
+		//删除选中项
+		dplItems.Items.RemoveAt(dplItems.SelectedIndex);
+		
+		DisplayInfoAndSetButtonEnabled();
+
+	}
+
+	protected void btnClear_Click(object sender, System.EventArgs e)
+	{
+		//清除所有选项
+		dplItems.Items.Clear();
+		
+		DisplayInfoAndSetButtonEnabled();
+	}
+
+	private void DisplayInfoAndSetButtonEnabled()
+	{
+		//如果当前没有项目，进行错误提示
+		if (dplItems.Items.Count == 0)
+		{
+			lblInfo.Text = "目前下拉菜单中没有任何选项";
+		}
+		else
+		{
+			//显示相应信息
+			lblInfo.Text = "目前下拉菜单中共有 " + dplItems.Items.Count + " 个选项<br>"
+				+ "当前选中的选项是第 " + (dplItems.SelectedIndex + 1) + " 项<br>"
+				+ "当前选中的项目的Text属性值为 " + dplItems.SelectedItem.Text
+				+ "， Value属性值为 " + dplItems.SelectedItem.Value;
+		}
+
+		//若当前没有项目，设置 "删除" 与 "清除" 按钮不可用
+		if (dplItems.Items.Count == 0)
+		{
+			btnDelete.Enabled = false;
+			btnClear.Enabled = false;
+		}
+		//否则设为可用
+		else
+		{
+			btnDelete.Enabled = true;
+			btnClear.Enabled = true;
+		}
+
+	}
+}

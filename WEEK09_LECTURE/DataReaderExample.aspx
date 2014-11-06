a2a9bf7a9bd4e4cb5485a4c8e04c745be368438e
ningboliuwei <liuwei@nbu.edu.cn>
2014-11-06 09:50:52 +0800

    上课时临时的一些改进（补充时间）

--- WEEK09_LECTURE/DataReaderExample.aspx
+++ WEEK09_LECTURE/DataReaderExample.aspx
@@ -11,6 +11,20 @@
     <form id="form1" runat="server">
     <div>
     
+        <!--<asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem Text="Student"></asp:ListItem>
+            <asp:ListItem Text="Course"></asp:ListItem>
+            <asp:ListItem Text="SC"></asp:ListItem>
+        </asp:DropDownList>
+        <asp:DropDownList ID="DropDownList2" runat="server">
+            <asp:ListItem Text="男"></asp:ListItem>
+            <asp:ListItem Text="女"></asp:ListItem>
+            <asp:ListItem Text="不限"></asp:ListItem>
+        </asp:DropDownList>-->
+        
+        <br/>
+        <asp:Button ID="btnExecute" runat="server" Text="执行" OnClick="btnExecute_Click"/>
+    
     </div>
     </form>
 </body>
--- WEEK09_LECTURE/SqlCommandExample.aspx.cs
+++ WEEK09_LECTURE/SqlCommandExample.aspx.cs
@@ -21,20 +21,20 @@ public partial class SqlCommandExample : System.Web.UI.Page
 			try
 			{
 				//第一步
-				//sqlConnection.Open();
-				//SqlCommand sqlCommand = new SqlCommand();
-				//sqlCommand.CommandText = "SELECT COUNT(*) FROM Student";
-				//sqlCommand.Connection = sqlConnection;
+                //sqlConnection.Open();
+                //SqlCommand sqlCommand = new SqlCommand();
+                //sqlCommand.CommandText = "SELECT COUNT(*) FROM Student";
+                //sqlCommand.Connection = sqlConnection;
 
-				//string count = sqlCommand.ExecuteScalar().ToString();
-				//Response.Write(string.Format("共有学生{0}名。", count));
+                //string count = sqlCommand.ExecuteScalar().ToString();
+                //Response.Write(string.Format("共有学生{0}名。", count));
 
-				//第二步，执行任意的 SQL 语句
-				//sqlConnection.Open();
-				//SqlCommand sqlCommand = new SqlCommand(txtCommand.Text, sqlConnection); //另一种写法
-				//sqlCommand.ExecuteNonQuery();
-				Response.Write("命令执行成功");
-			}
+                ////第二步，执行任意的 SQL 语句
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand(txtCommand.Text, sqlConnection); //另一种写法
+                sqlCommand.ExecuteNonQuery();
+                Response.Write("命令执行成功");
+            }
 			catch (Exception ex)
 			{
 				Response.Write(ex.Message);
